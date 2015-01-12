af075bc9c3ee8517a08b7a27ba8943719e9b6806
kesselbernd <nico@selbmann.info>
2015-01-12 15:59:23 +0100

    255

--- dht11.py
+++ dht11.py
@@ -3,10 +3,10 @@
 import RPi.GPIO as GPIO
 #import time
 
-class TempSensor(object):
-    def __init__(self, gpio_temp):
-        self.__gpio_temp = gpio_temp
-        GPIO.setup(self.__gpio_temp, GPIO.IN)
+class DHT11(object):
+    def __init__(self, gpio_dht11):
+        self.__gpio_dht11 = gpio_dht11
+        GPIO.setup(self.__gpio_dht11, GPIO.IN)
 
 """
 import RPi.GPIO as GPIO
