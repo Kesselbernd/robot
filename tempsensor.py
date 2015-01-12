666e7627bf63bc361ba475d06caeb5a24644cc5a
kesselbernd <nico@selbmann.info>
2015-01-12 14:25:19 +0100

    255

--- tempsensor.py
+++ tempsensor.py
@@ -4,4 +4,6 @@ import RPi.GPIO as GPIO
 import time
 
 class TempSensor(object):
-    pass
+    def __init__(self, gpio_temp):
+        self.__gpio_temp = gpio_temp
+        GPIO.setup(self.__gpio_temp, GPIO.IN)
