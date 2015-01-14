98e46ae9123355c2867ec5c5ed97b6ea3bdf8838
kesselbernd <nico@selbmann.info>
2015-01-14 09:41:52 +0100

    RTK-000-001

--- main.py
+++ main.py
@@ -7,6 +7,19 @@ from robot import *
 #from sensor import *
 from led import *
 
+#Merkliste Verwendung GPIO
+GPIO_Pins = {
+    "led1" : 1,
+    "led2" : 1,
+    "trigger" : 1,
+    "echo" : 1,
+    "dht11" : 1,
+    "motor1_pin1" : 17,
+    "motor1_pin2" : 18,
+    "motor2_pin1" : 22,
+    "motor2_pin2" : 23,
+    }
+
 try:
     while True:
         #Robot(gpio_trigger, gpio_echo, sicherheitsabstand)
