e4b9d750cf447a870ab56f74b1e44a4ba8a88923
kesselbernd <nico@selbmann.info>
2015-01-15 14:55:02 +0100

    RTK-000-001

--- camera.py
+++ camera.py
@@ -2,9 +2,25 @@
 
 import RPi.GPIO as GPIO
 import time
-#import random
+from picamera import *
 
 GPIO.setmode(GPIO.BCM)
 
-class CAMERA(object):
-    pass
+class CAMERA(PiCamera):
+    def __init__(self):
+
+    def foto(self):
+        self.resolution = (1024, 768)
+        self.start_preview()
+        # Camera warm-up time
+        time.sleep(2)
+        zeit = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+        self.capture('bild_' + zeit + '.jpg')
+
+    def stream(self):
+        pass
+
+#Install on PI python-picamera python3-picamera python-picamera-docs
+#http://www.raspberrypi.org/learning/python-picamera-setup/
+#http://picamera.readthedocs.org/en/release-1.9/recipes1.html
+
